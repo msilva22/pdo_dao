0c395bf7bb3bac5e71efa4fc17e32d946249cb05
Moisés Silva <moises.ambiental@hotmail.com>
2024-02-09 19:09:42 -0300

    etodos de lista.

--- index.php
+++ index.php
@@ -2,14 +2,23 @@
 
 require_once("config.php");
 
-/*$sql = new Sql();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo json_encode($usuarios);*/
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
-$root = new Usuario();
-$root->loadById(3);
-echo $root;
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "12345");
+
+echo $usuario;
 
 ?>
